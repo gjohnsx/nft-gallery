643c6105cc42f1c47fc87096dc2d15229a186bde
gjohnsx <gregjohns@fastmail.com>
2022-08-03 22:50:21 -0400

    added state variables

--- pages/mint.js
+++ pages/mint.js
@@ -4,8 +4,17 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import myEpicNft from '../utils/MyEpicNFT.json';
 
+const CONTRACT_ADDRESS = "0xE748E5b19891C9ed0eC1dcd358966168f5f156FA";
+
 export default function Mint() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [mintedNFT, setMintedNFT] = useState(null);
+	const [miningStatus, setMiningStatus] = useState(null);
+	const [loadingState, setLoadingState] = useState(0);
+	const [txError, setTxError] = useState(null);
+  const [correctNetwork, setCorrectNetwork] = useState(false);
+  const [numNftsMinted, setNumNftsMinted] = useState(0);
+  const [maxSupply, setMaxSupply] = useState(0);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -57,12 +66,9 @@ export default function Mint() {
         const signer = provider.getSigner();
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-        // THIS IS THE MAGIC SAUCE.
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/goerli/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
         });
 
         console.log("Setup event listener!")
@@ -75,9 +81,45 @@ export default function Mint() {
     }
   }
 
-  const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xa1aF9b538cc411F67DcEFEbB874B74160073d193";
+  const getMaxSupply = async () => {
+    try {
+      const { ethereum } = window;
   
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+        const maxSupplyFromContract = await connectedContract.getMaxSupply();
+        setMaxSupply(parseInt(maxSupplyFromContract, 16));
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getNftsMintedSoFar = async () => {
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+        const mintedSoFar = await connectedContract.getTotalNFTsMintedSoFar();
+        setNumNftsMinted(parseInt(mintedSoFar, 16));
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
     try {
       const { ethereum } = window;
   
@@ -93,7 +135,8 @@ export default function Mint() {
         await nftTxn.wait();
         
         console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
-  
+        const mintedSoFar = await connectedContract.getTotalNFTsMintedSoFar();
+        setNumNftsMinted(parseInt(mintedSoFar, 16));
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -101,7 +144,6 @@ export default function Mint() {
       console.log(error)
     }
   }
-  
 
   const renderNotConnectedContainer = () => (
     <button
@@ -114,7 +156,17 @@ export default function Mint() {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    console.log('getting max supply only on page load', '*'.repeat(10));
+    getMaxSupply();
+    getNftsMintedSoFar();
   }, []);
+
+  useEffect(() => {
+    getNftsMintedSoFar();
+  }, [numNftsMinted])
+
+  console.log('minted so far =', numNftsMinted)
+  console.log('max supply =', maxSupply);
 
   return (
     <>
@@ -125,15 +177,20 @@ export default function Mint() {
           <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
             <span className="block">My NFT Collection</span>
           </h2>
-          <p className="mt-4 text-lg leading-6 text-blue-200">
+          <p className="mt-2 text-lg leading-6 text-blue-200">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
+          <div className="mt-2">
+            <h3 className="text-xl text-white sm:text-xl">
+              {numNftsMinted} / {maxSupply} minted
+            </h3>
+          </div>
 
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
             <button 
-            className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
+            className="my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50 sm:w-auto"
             onClick={askContractToMintNft} 
             >
               Mint NFT
